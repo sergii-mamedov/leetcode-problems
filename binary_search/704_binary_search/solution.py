14e718eb3f28c0725a6d1f5a65c0561eca9e30c7
Sergii Mamedov <finve.ua@gmail.com>
2026-04-30 11:45:05 +0200

    added solution for 704

---
+++ binary_search/704_binary_search/solution.py
@@ -0,0 +1,21 @@
+"""
+704. Binary search
+https://leetcode.com/problems/binary-search/description/
+https://neetcode.io/problems/binary-search/question
+"""
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (r + l) // 2
+
+            if nums[m] == target:
+                return m
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                l = m + 1
+        
+        return -1
